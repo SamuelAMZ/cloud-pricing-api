16ca780f06b598faebbd1d576201c6ae07321512
sam_dev <samueldevpro09@gmail.com>
2022-08-28 18:07:51 +0200

    state before starting gcp adding to db

--- scrap/data.js
+++ scrap/data.js
@@ -22,21 +22,19 @@ function bluePrint(providerData, data, name) {
   providerData.push(data);
 
   if (providerData.length === 4 && name === "linode") {
-    let obj = {};
-    obj["company"] = name;
-    obj["computer"] = providerData[0].compute;
-    obj["storage"] = providerData[1].storage;
-    obj["database"] = providerData[2].database;
-    obj["networking"] = providerData[3].networking;
-
-    console.log(obj);
-
-    db.collection("linode")
-      .insertOne(obj)
-      .then((res) => {
-        console.log(res);
-        console.log("added");
-      });
+    addingData(providerData, name);
+  }
+  if (providerData.length === 4 && name === "ovhCloud") {
+    addingData(providerData, name);
+  }
+  if (providerData.length === 4 && name === "vultr") {
+    addingData(providerData, name);
+  }
+  if (providerData.length === 4 && name === "digitalOcean") {
+    addingData(providerData, name);
+  }
+  if (providerData.length === 4 && name === "GCP") {
+    addingData(providerData, name);
   }
 }
 
@@ -60,6 +58,24 @@ function awsArr(data) {
 }
 function azureArr(data) {
   bluePrint(azureData, data, "azure");
+}
+
+// adding data to db func
+function addingData(providerData, name) {
+  let obj = {};
+  obj["company"] = name;
+  obj["computer"] = providerData[0].compute;
+  obj["storage"] = providerData[1].storage;
+  obj["database"] = providerData[2].database;
+  obj["networking"] = providerData[3].networking;
+
+  console.log(obj);
+
+  db.collection(name)
+    .insertOne(obj)
+    .then((res) => {
+      console.log(res);
+    });
 }
 
 module.exports = {
