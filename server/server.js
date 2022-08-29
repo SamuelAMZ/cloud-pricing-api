d96836c553cb5fb07ec11469205f7146b072bd78
sam_dev <samueldevpro09@gmail.com>
2022-08-29 05:10:31 +0200

    added a little internal desc to the endpoints

--- server/server.js
+++ server/server.js
@@ -15,12 +15,53 @@ app.get("/", (req, res) => {
   res.status(200).send("Server up");
 });
 
+/*   
+    @desc: get linode data {compute-database-storage-netwokring}
+    @method: GET
+    @endpoint: /api/v1/linode
+*/
 app.use("/api/v1/linode", linodeRoute);
+
+/*   
+    @desc: get ovh data {compute-database-storage-netwokring}
+    @method: GET
+    @endpoint: /api/v1/ovh
+*/
 app.use("/api/v1/ovh", ovhRoute);
+
+/*   
+    @desc: get vultr data {compute-database-storage-netwokring}
+    @method: GET
+    @endpoint: /api/v1/vultr
+*/
 app.use("/api/v1/vultr", vultrRoute);
+
+/*   
+    @desc: get digital ocean data {compute-database-storage-netwokring}
+    @method: GET
+    @endpoint: /api/v1/digitalOcean
+*/
 app.use("/api/v1/digitalOcean", digitalOceanRoute);
+
+/*   
+    @desc: get gcp data {compute-database-storage-netwokring}
+    @method: GET
+    @endpoint: /api/v1/gcp
+*/
 app.use("/api/v1/gcp", gcpRoute);
+
+/*   
+    @desc: get aws data {compute-database-storage-netwokring}
+    @method: GET
+    @endpoint: /api/v1/aws
+*/
 app.use("/api/v1/aws", awsRoute);
+
+/*   
+    @desc: get azure data {compute-database-storage-netwokring}
+    @method: GET
+    @endpoint: /api/v1/azure
+*/
 app.use("/api/v1/azure", azureRoute);
 
 app.listen(process.env.PORT, () =>
