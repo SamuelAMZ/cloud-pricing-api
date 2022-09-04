1a425637c7085ffdda2398348b95c84c622e4430
sam_dev <samueldevpro09@gmail.com>
2022-09-04 09:46:06 +0200

    fix little checking issue with already generated tokens

--- routes/api/generateToken.js
+++ routes/api/generateToken.js
@@ -6,24 +6,24 @@ const Token = require("../../models/ApiToken");
 genTokenRouter.post("/", async (req, res) => {
   const uId = res.locals.user._id;
 
-  const token = await generateToken(uId);
+  // check if user do not already have a token
+  const alreadyHaveToken = await Token.findOne({ user: uId });
+  if (alreadyHaveToken === null || !alreadyHaveToken) {
+    // generate token
+    const token = await generateToken(uId);
 
-  if (token._message) {
-    res
-      .status(500)
-      .json({ message: "cannot generate token", error: token._message });
-  } else {
-    // check if user do not already have a token
-    const alreadyHaveToken = await Token.findById(token.tokenSaved._id);
-    if (alreadyHaveToken === null || !alreadyHaveToken) {
-      // generate token
+    if (token._message) {
+      res
+        .status(500)
+        .json({ message: "cannot generate token", error: token._message });
+    } else {
       res
         .status(200)
         .json({ message: "token generated", token: token.hashedToken });
-    } else {
-      // if found any record so the user have aleady token
-      res.status(400).json({ message: "already have token" });
     }
+  } else {
+    // if found any record so the user have aleady token
+    res.status(400).json({ message: "already have token" });
   }
 });
 
